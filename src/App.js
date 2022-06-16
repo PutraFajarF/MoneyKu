9fa9bbb8a9140e4e86237c6044c13dc7be134385
Putra Fajar <putrautalls@gmail.com>
2022-06-17 01:39:56 +0700

    added Navbar.js Component and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 
 // Page Component
 import Home from './pages/home/Home';
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route index path='/' element={<Home/>} />
           <Route path='login' element={<Login />}/>
