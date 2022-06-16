a16f2809d847d329ddf47add4e6191b83cd0745a
Putra Fajar <putrautalls@gmail.com>
2022-06-16 23:51:40 +0700

    creating routes and pages, adding react-router-dom to depedencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// Page Component
+import Home from './pages/home/Home';
+import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/' element={<Home/>} />
+          <Route path='login' element={<Login />}/>
+          <Route path='signup' element={<Signup />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
