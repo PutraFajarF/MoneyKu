ec7722e65cb73ac69e6ff25f906ce81b558d932c
Putra Fajar <putrautalls@gmail.com>
2022-06-21 22:55:25 +0700

    use conditional statement to render App.js when authIsReady, when we get state of user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // Page Component
 import Home from './pages/home/Home';
@@ -7,16 +8,19 @@ import Login from './pages/login/Login';
 import Navbar from './components/Navbar';
 
 function App() {
+  const { authIsReady } = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <Routes>
-          <Route index path='/' element={<Home/>} />
-          <Route path='login' element={<Login />}/>
-          <Route path='signup' element={<Signup />}/>
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route index path='/' element={<Home/>} />
+            <Route path='login' element={<Login />}/>
+            <Route path='signup' element={<Signup />}/>
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
