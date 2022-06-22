aa043414f57e16280399e7a8e1308f99c321a776
Putra Fajar <putrautalls@gmail.com>
2022-06-22 23:17:29 +0700

    making TransactionForm.js and render it in Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,18 @@
-import styles from './Home.module.css';
+import TransactionForm from './TransactionForm';
 
+// styles
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        Transaction List
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
   )
 }
 
