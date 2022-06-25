dfb6c1e41b285898efe401ade14025159373989d
Putra Fajar <putrautalls@gmail.com>
2022-06-25 23:41:31 +0700

    create TransactionList.js Component and apply useCollection.js Hook to Home.js Page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,21 @@
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 // Components
 import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 
 // styles
 import styles from './Home.module.css';
 
 const Home = () => {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection();
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        Transaction List
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
       </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid}/>
