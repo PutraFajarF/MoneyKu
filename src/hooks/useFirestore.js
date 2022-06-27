c1678fb75bd778530b8dc655e7d9eb55247d2992
Putra Fajar <putrautalls@gmail.com>
2022-06-27 21:26:01 +0700

    adding delete transaction function in useFirestore.js Hook and apply it to TransactionList.js

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -16,6 +16,8 @@ const firestoreReducer = (state, action) => {
       return {success: false, isPending: false, error: action.payload, document: null};
     case "ADDED_DOCUMENT":
       return {success: true, isPending: false, error: null, document: action.payload};
+    case "DELETED_DOCUMENT":
+      return {success: true, isPending: false, error: null, document: null};
     default:
       return state;
   }
@@ -51,7 +53,14 @@ export const useFirestore = (collection) => {
 
   // delete a document
   const deleteDocument = async (id) => {
+    dispatch({ type: 'IS_PENDING'});
 
+    try {
+      await ref.doc(id).delete();
+      dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: 'could not delete' });
+    }
   };
 
   useEffect(() => {
